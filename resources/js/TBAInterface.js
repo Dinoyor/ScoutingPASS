4f98ba880ffaad35125d9b1d0636893e8cd22ce2
Dinoyor <163239545+Dinoyor@users.noreply.github.com>
2024-03-15 06:59:25 -0700

    Update TBAInterface.js

--- resources/js/TBAInterface.js
+++ resources/js/TBAInterface.js
@@ -8,7 +8,7 @@ var authKey = "uTHeEfPigDp9huQCpLNkWK7FBQIb01Qrzvt4MAjh9z2WQDkrsvNE77ch6bOPvPb6"
  * @param {eventCode} eventCode the event code (i.e. 2020caln) to pull the team list
  */
 function getTeams(eventCode) {
-	if (authKey) {
+	if (authKey) {2024wayak
 		var xmlhttp = new XMLHttpRequest();
 		var url = "https://www.thebluealliance.com/api/v3/event/" + eventCode + "/teams/simple";
 		xmlhttp.open("GET", url, true);
